112428e45c1d168661cfd245033d7b331936f6a7
Andrew Chan <46029314+AndrewChan168@users.noreply.github.com>
2019-11-01 00:45:00 +0800

    Add files via upload

--- Java8ForProgrammers/Section07/CardShuffling/src/DeckOfCards.java
+++ Java8ForProgrammers/Section07/CardShuffling/src/DeckOfCards.java
@@ -12,8 +12,39 @@ public class DeckOfCards {
     public DeckOfCards(){
         String[] faces = { "Ace", "Deuce", "Three", "Four", "Five",  "Seven",
                 "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
 
-        
+        // create array of Card objects
+        deck = new Card[NUMBER_OF_CARDS];
+        currentCard = 0;
+
+        for(int counter=0; counter<NUMBER_OF_CARDS; counter++){
+            deck[counter] = new Card(faces[counter%13], faces[counter/13]);
+        }
     }
 
+    // shuffle all cards
+    public void shuffle(){
+        currentCard = 0;
+
+        // for each Card, pick another random Card (0-51) and swap them
+        for(int first=0; first<deck.length; first++){
+            // select a random number between 0 and 51
+            int second = randomNumbers.nextInt(NUMBER_OF_CARDS);
+
+            // swap current Card with randomly selected Card
+            Card temp = deck[first];
+            deck[first] = deck[second];
+            deck[second] = temp;
+        }
+    }
+
+    public Card dealCard(){
+        // determine whether Cards remain to be dealt
+        if (currentCard<deck.length){
+            return deck[currentCard++];
+        } else {
+            return null;
+        }
+    }
 }
